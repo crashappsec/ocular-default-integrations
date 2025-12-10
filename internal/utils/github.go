d8bf2e4e8b163f4e5edd2e22c93620f7921a7d20
Bryce Thuilot <bryce@crashoverride.com>
2025-12-10 14:42:03 -0500

    fix: retrieve github installation token dynmically
    Signed-off-by: Bryce Thuilot <bryce@crashoverride.com>

--- internal/utils/github.go
+++ internal/utils/github.go
@@ -14,9 +14,10 @@ import (
 	"net/http"
 
 	"github.com/bradleyfalzon/ghinstallation/v2"
+	"github.com/google/go-github/v71/github"
 )
 
-func AuthenticateGitHubApp(_ context.Context, appID, installationID int64, privatePEM []byte) (*ghinstallation.Transport, error) {
+func AuthenticateGitHubAppInstallation(_ context.Context, appID, installationID int64, privatePEM []byte) (*ghinstallation.Transport, error) {
 	// Shared transport to reuse TCP connections.
 	tr := http.DefaultTransport
 	// Wrap the shared transport for use with the app ID 1 authenticating with installation ID 99.
@@ -25,4 +26,44 @@ func AuthenticateGitHubApp(_ context.Context, appID, installationID int64, priva
 		log.Fatal(err)
 	}
 	return itr, nil
+}
+
+func AuthenticateGitHubAppForRepository(ctx context.Context, org, repo string, appID int64, privatePEM []byte) (*ghinstallation.Transport, error) {
+	transport, err := ghinstallation.NewAppsTransport(http.DefaultTransport, appID, privatePEM)
+	if err != nil {
+		return nil, err
+	}
+	ghClient := github.NewClient(&http.Client{Transport: transport})
+
+	installation, _, err := ghClient.Apps.FindRepositoryInstallation(ctx, org, repo)
+	if err != nil {
+		return nil, err
+	}
+	return AuthenticateGitHubAppInstallation(ctx, appID, installation.GetID(), privatePEM)
+}
+
+func AuthenticateGitHubAppForOrg(ctx context.Context, org string, appID int64, privatePEM []byte) (*ghinstallation.Transport, error) {
+	transport, err := ghinstallation.NewAppsTransport(http.DefaultTransport, appID, privatePEM)
+	if err != nil {
+		return nil, err
+	}
+	ghClient := github.NewClient(&http.Client{Transport: transport})
+	installation, _, err := ghClient.Apps.FindOrganizationInstallation(ctx, org)
+	if err != nil {
+		return nil, err
+	}
+	return AuthenticateGitHubAppInstallation(ctx, appID, installation.GetID(), privatePEM)
+}
+
+func AuthenticateGitHubAppForUser(ctx context.Context, user string, appID int64, privatePEM []byte) (*ghinstallation.Transport, error) {
+	transport, err := ghinstallation.NewAppsTransport(http.DefaultTransport, appID, privatePEM)
+	if err != nil {
+		return nil, err
+	}
+	ghClient := github.NewClient(&http.Client{Transport: transport})
+	installation, _, err := ghClient.Apps.FindUserInstallation(ctx, user)
+	if err != nil {
+		return nil, err
+	}
+	return AuthenticateGitHubAppInstallation(ctx, appID, installation.GetID(), privatePEM)
 }
